3f12908f8b71b242a74595eda424aaa59b2534a2
Johnson Pan <jpan@int.funkygeek.me>
2017-05-24 00:13:50 -0700

    A1 part 3

--- a1/src/a1/a1.go
+++ a1/src/a1/a1.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"fmt"
 	"io/ioutil"
+	"errors"
 )
 
 func isPrime(x int) bool{
@@ -51,4 +52,59 @@ func countStrings(filename string) map[string]int{
 		}
 	}
 	return dict
+}
+
+type Time24 struct {
+    hour, minute, second uint8
+}
+
+func equalsTime24(t Time24,otherT Time24) bool {
+    if t.hour==otherT.hour && t.minute==otherT.minute && t.second==otherT.second{
+		return true
+	}
+	return false
+}
+
+func lessThanTime24(t Time24,otherT Time24) bool {
+	var t1 uint32 = t.getTick()
+	var t2 uint32 = otherT.getTick()
+	if t1 < t2{
+		return true
+	}
+    return false
+}
+
+func (t Time24) getTick() uint32 {
+	return uint32(t.hour)*3600 +uint32(t.minute)*60 +uint32(t.second)
+}
+
+func (t Time24) String() string {
+	var str = fmt.Sprintf("%02d:%02d:%02d", t.hour, t.minute, t.second)
+	return str;
+}
+
+func (t Time24) validTime24() bool{
+	if t.hour >= 0 && t.hour <24 &&
+	t.minute >= 0 && t.minute <60 &&
+	t.second >= 0 && t.second <60 {
+		return true
+	}
+	return false
+}
+
+func minTime24(times []Time24) (Time24, error){
+	if len(times)==0 {
+		error := errors.New("Encountered empty Time24 array")
+		return Time24 { hour:0, minute:0, second:0 },error
+	}
+
+	var minIndex int = 0
+	var minTick uint32 = times[0].getTick()
+	for i, element := range times {
+		if element.getTick() < minTick{
+			minIndex = i
+			minTick=element.getTick()
+		}
+	}
+	return times[minIndex],nil
 }
--- a1/src/a1/a1_test.go
+++ a1/src/a1/a1_test.go
@@ -72,3 +72,95 @@ func TestCountStrings(t *testing.T) {
 		t.Error("Failed")
 	}
 }
+
+
+func TestEqualsTime24(t *testing.T) {
+	var error bool = false
+
+	var t1 Time24 = Time24 { hour:2, minute:35, second:27 }
+	var t2 Time24 = Time24 { hour:2, minute:35, second:27 }
+	var t3 Time24 = Time24 { hour:2, minute:35, second:28 }
+	if equalsTime24(t1,t2)!=true{
+		error = true
+	}
+	if equalsTime24(t1,t3)!=false{
+		error = true
+	}
+	if error{
+		t.Error("Failed")
+	}
+}
+
+func TestLessThanTime24(t *testing.T) {
+	var error bool = false
+
+	var t1 Time24 = Time24 { hour:2, minute:35, second:27 }
+	var t2 Time24 = Time24 { hour:2, minute:35, second:27 }
+	var t3 Time24 = Time24 { hour:2, minute:35, second:28 }
+	if lessThanTime24(t1,t3)!=true{
+		error = true
+	}
+	if lessThanTime24(t1,t2)!=false{
+		error = true
+	}
+	if error{
+		t.Error("Failed")
+	}
+}
+
+func TestTime24ToString(t *testing.T){
+	var error bool = false
+	
+	var t1 Time24 = Time24 { hour:2, minute:35, second:27 }
+	fmt.Println(t1)
+	if t1.String()!="02:35:27"{
+		error = true
+	}
+	if error{
+		t.Error("Failed")
+	}
+}
+
+func TestValidTime24(t *testing.T){
+	var error bool = false
+	
+	var t1 Time24 = Time24 { hour:2, minute:35, second:27 }
+	var t2 Time24 = Time24 { hour:2, minute:35, second:80 }
+	var t3 Time24 = Time24 { hour:30, minute:35, second:0 }
+	
+	if t1.validTime24()!=true{
+		fmt.Printf("Test case failed on %v",t1)
+		error = true
+	}
+	if t2.validTime24()!=false{
+		fmt.Printf("Test case failed on %v",t2)
+		error = true
+	}
+	if t3.validTime24()!=false{
+		fmt.Printf("Test case failed on %v",t3)
+		error = true
+	}
+	if error{
+		t.Error("Failed")
+	}
+}
+
+func TestMinTime24(t *testing.T){
+	var error bool = false
+	var t1 Time24 = Time24 { hour:2, minute:35, second:27 }
+	var t2 Time24 = Time24 { hour:12, minute:45, second:4 }
+	var t3 Time24 = Time24 { hour:23, minute:35, second:0 }
+
+	if _,err := minTime24([]Time24{}) ; err==nil{
+		fmt.Println("Test case failed on empty array.")
+		error = true
+	}
+	if minTime,err := minTime24([]Time24{t2,t1,t3}) ; err!=nil || !equalsTime24(minTime,t1) {
+		fmt.Println("Test case failed when comparing t2,t1,t3.")
+		error = true
+	}
+	if error{
+		t.Error("Failed")
+	}
+}
+
