bbd1837738546c1accc22520fcffb364ed44e75b
johnsonp <johnsonp@sfu.ca>
2017-08-02 21:30:41 -0700

    A5 q15

--- a5/a5.hs
+++ a5/a5.hs
@@ -1,18 +1,22 @@
+-- 1
 snoc :: a -> [a] -> [a]
 snoc x [] = [x]
 snoc x y = (head y) : snoc x (tail y)
 
 
+-- 2
 myappend :: [a] -> [a] -> [a]
 myappend x [] = x
 myappend x y = myappend (snoc (head y) x) (tail y)
 
 
+-- 3
 myreverse :: [a] -> [a]
 myreverse [] = []
 myreverse x = (last x):(myreverse (init x))
 
 
+-- 4
 is_prime :: Int -> Bool
 is_prime n 
     | n < 2 = False
@@ -27,13 +31,95 @@ count_emirps n
     | otherwise = length [x | x <- [13..n], is_emirp x]
 
 
+-- 5
 biggest_sum :: [[Int]] -> [Int]
 biggest_sum [x] = x 
 biggest_sum lst = if sum (head lst) > sum (biggest_sum (tail lst)) then head lst else biggest_sum (tail lst)
 
 
+-- 6
 greatest :: (a -> Int) -> [a] -> a
 greatest f [x] = x 
 greatest f lst = if f (head lst) > f (greatest f (tail lst) ) then head lst else greatest f (tail lst)
 
 
+-- 7
+is_bit :: (Integral a) => a -> Bool
+is_bit 1 = True
+is_bit 0 = True
+is_bit x = False
+
+
+-- 8
+flip_bit :: (Integral a) => a -> a
+flip_bit x = if is_bit x then (if x == 0 then 1 else 0) else error "Input is not a bit."
+
+
+-- 9
+is_bit_seq1 :: (Integral a) => [a] -> Bool
+is_bit_seq1 [] = True
+is_bit_seq1 lst
+    | is_bit (head lst) = True && is_bit_seq1 (tail lst)
+    | otherwise = False
+
+is_bit_seq2 :: (Integral a) => [a] -> Bool
+is_bit_seq2 [] = True
+is_bit_seq2 lst = if is_bit (head lst) then True && is_bit_seq2 (tail lst) else False
+
+is_bit_seq3 :: (Integral a) => [a] -> Bool
+is_bit_seq3 [] = True
+is_bit_seq3 lst = all is_bit lst
+
+
+-- 10
+invert_bits1 :: (Integral a) => [a] -> [a]
+invert_bits1 [] = []
+invert_bits1 lst = flip_bit (head lst):invert_bits1(tail lst)
+
+invert_bits2 :: (Integral a) => [a] -> [a]
+invert_bits2 [] = []
+invert_bits2 lst = map flip_bit lst
+
+invert_bits3 :: (Integral a) => [a] -> [a]
+invert_bits3 [] = []
+invert_bits3 lst = [flip_bit x | x <- lst]
+
+
+-- 11
+bit_count :: (Integral a) => [a] -> (Int, Int)
+bit_count [] = (0, 0)
+bit_count lst = (length [x|x <- lst, x == 0], length [x|x <- lst, x == 1])
+
+
+-- 12
+all_basic_bit_seqs :: (Integral a) => a -> [[Int]]
+all_basic_bit_seqs n
+    | n < 1 = []
+    | n == 1 = [[0],[1]]
+    | otherwise = myappend [0:x | x <- all_basic_bit_seqs (n-1) ] [1:x | x <- all_basic_bit_seqs (n-1) ]
+
+
+
+data Bit = Zero | One
+    deriving (Show, Eq)
+
+-- 13
+flipBit :: Bit -> Bit
+flipBit Zero = One
+flipBit One = Zero
+
+
+-- 14
+invert :: [Bit] -> [Bit]
+invert [] = []
+invert lst = flipBit (head lst) : invert (tail lst)
+
+
+
+all_bit_seqs :: (Integral a) => a -> [[Bit]]
+all_bit_seqs n
+    | n < 1 = []
+    -- I came up with this solution based on getting binary digit by continuous division.
+    | otherwise = [ [ if ((x `quot` (2^q)) `mod` 2) == 0 then Zero else One | q<-[n-1,n-2..0]]  | x <- [0..2^n-1]]
+    
+-- [mod (quot x (2^q)) 2 | q<-[n-1,n-2..0]]
